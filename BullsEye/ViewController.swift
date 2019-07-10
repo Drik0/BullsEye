7cbdc6bb5d04d205ea7aca9a016a18e3bc6866cf
Gerson Costa <gerson.lcc@hotmail.com>
2019-07-10 05:26:03 +0100

    UI design

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetValueLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startNewRound()
     }
 
 
     @IBAction func showAlert(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Hello World!", message: "This is my first app!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Awesome!", style: .default, handler: nil))
+        let alert = UIAlertController(title: "Hello World!", message: "The value of the slider is now \(currentValue)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Awesome", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        currentValue = Int(sender.value.rounded())
+    }
+    
+    func startNewRound() {
+        currentValue = 50
+        targetValue = Int.random(in: 1...100)
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetValueLabel.text = "\(targetValue)"
     }
 }
 
